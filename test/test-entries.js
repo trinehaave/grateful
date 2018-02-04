ee499eea695cdcfeb674b7356793a0a4a81553fe
trinehaave <trinehaave@gmail.com>
2018-02-04 19:36:59 +0100

    .env fix 🙈

--- test/test-entries.js
+++ test/test-entries.js
@@ -1,10 +1,11 @@
+const dotenv = require('dotenv')
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const faker = require('faker')
 const mongoose = require('mongoose')
-const {
-  TEST_DATABASE_URL
-} = require('../config')
+const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
+
+dotenv.config({path: './variables.env'})
 
 mongoose.Promise = global.Promise
 
