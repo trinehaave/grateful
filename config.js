e95b0c8cf0c7db3549eb99263b837d7fa3cf477a
trinehaave <trinehaave@gmail.com>
2018-02-04 15:41:07 +0100

    URL changes 🐨

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://admin:admin@ds223578.mlab.com:23578/production-test',
   PORT: process.env.PORT,
   SECRET: process.env.SECRET
 }
