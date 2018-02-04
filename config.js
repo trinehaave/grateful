3fce39c88164ccdb939de81913fa93c0b96667dd
trinehaave <trinehaave@gmail.com>
2018-02-04 15:56:58 +0100

    travis CI 🐮

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
-  SECRET: process.env.SECRET
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://admin:admin@ds123258.mlab.com:23258/production-journal',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://admin:admin@ds223578.mlab.com:23578/production-test',
+  SECRET: process.env.SECRET || 'this'
 }
